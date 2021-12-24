4e32c984213f1ebb1841f86239603d61149c27fc
DSieradzki <daw.sieradzki@gmail.com>
2021-12-24 12:37:30 +0100

    add TaskGroup entity

--- src/main/java/pl/dsieradzki/taskprojecttodoapp/model/TaskGroup.java
+++ src/main/java/pl/dsieradzki/taskprojecttodoapp/model/TaskGroup.java
@@ -2,37 +2,29 @@ package pl.dsieradzki.taskprojecttodoapp.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "tasks")
-public class Task {
+@Table(name = "task_groups")
+public class TaskGroup {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotBlank(message = "Task's description can't be blank")
+    @NotBlank(message = "Task's groups description can't be blank")
     private String description;
     private boolean done;
-    private LocalDateTime deadline;
     @Embedded
     private Audit audit = new Audit();
-    public Task() {
+
+    public TaskGroup() {
     }
 
-    public LocalDateTime getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDateTime deadline) {
-        this.deadline = deadline;
-    }
 
     public int getId() {
         return id;
     }
 
-     void setId(int id) {
+    void setId(int id) {
         this.id = id;
     }
 
@@ -50,12 +42,6 @@ public class Task {
 
     public void setDone(boolean done) {
         this.done = done;
-    }
-
-    public void updateFrom(Task source){
-        description = source.description;
-        done = source.done;
-        deadline = source.deadline;
     }
 
 }
